811cf7b723b421a8d8438da8ad5f0e542f757987
myknow <myknow000@gmail.com>
2026-01-21 20:04:36 +0900

    feat: 하드코딩 제거 및 주석 추가

--- src/main/java/bank/BankService.java
+++ src/main/java/bank/BankService.java
@@ -3,7 +3,10 @@ package bank;
 public class BankService {
     private static final int MAX_SIZE = 10;
 
-    private Account[] accounts;
+    // accounts 자체를 재할당 받을 일이 없으므로 final 키워드 사용
+    private final Account[] accounts;
+
+    // 현재 계좌 개수를 확인하기 위한 변수
     private int count;
 
     BankService() {
@@ -13,16 +16,16 @@ public class BankService {
 
     public void addAccount(Account account) {
         // 3. 계좌 최대 개수를 초과하면 추가를 무시한다.
-        if (count < 10) {
+        if (count < MAX_SIZE) {
             accounts[count++] = account;
         }
     }
 
     public int getTotalBalance() {
         int total = 0;
-        for (Account account: accounts) {
-            if (account == null) break;
-            total += account.getBalance();
+        // foreach로 null check 하는 대신 count로 처리
+        for (int i = 0; i < count; i++) {
+            total += accounts[i].getBalance();
         }
         return total;
     }
