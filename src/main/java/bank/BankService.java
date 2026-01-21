2878419a2be035d1a35dde1c9b00784b2e468fe7
myknow <myknow000@gmail.com>
2026-01-21 19:43:32 +0900

    feat: Test 코드에 대응하는 기능 구현

--- src/main/java/bank/BankService.java
+++ src/main/java/bank/BankService.java
@@ -1,4 +1,29 @@
 package bank;
 
 public class BankService {
+    private static final int MAX_SIZE = 10;
+
+    private Account[] accounts;
+    private int count;
+
+    BankService() {
+        count = 0;
+        accounts = new Account[MAX_SIZE];
+    }
+
+    public void addAccount(Account account) {
+        // 3. 계좌 최대 개수를 초과하면 추가를 무시한다.
+        if (count < 10) {
+            accounts[count++] = account;
+        }
+    }
+
+    public int getTotalBalance() {
+        int total = 0;
+        for (Account account: accounts) {
+            if (account == null) break;
+            total += account.getBalance();
+        }
+        return total;
+    }
 }
--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -1,4 +1,15 @@
 package bank;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account{
+    private double interestRate;
+
+    public SavingsAccount(String owner, int balance, double interestRate) {
+        super(owner, balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    void deposit(int amount) {
+        super.balance += (int) ((1.0 + interestRate) * (double) amount);
+    }
 }
