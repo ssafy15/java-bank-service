2beb80ef6ae9279bb17b84586fa1b157795dec5b
myknow <myknow000@gmail.com>
2026-01-21 20:04:45 +0900

    feat: 주석 추가

--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -1,7 +1,9 @@
 package bank;
 
 public class SavingsAccount extends Account{
-    private double interestRate;
+    // interestRate가 변경되어야 한다는 요구사항이 없으므로 final 키워드 사용
+    // 외부에서 interestRate에 접근해야 하는 요구사항이 없으므로 접근 제한자로 private를 채택하여 캡슐화.
+    private final double interestRate;
 
     public SavingsAccount(String owner, int balance, double interestRate) {
         super(owner, balance);
@@ -10,6 +12,8 @@ public class SavingsAccount extends Account{
 
     @Override
     void deposit(int amount) {
-        super.balance += (int) ((1.0 + interestRate) * (double) amount);
+        final double interest = interestRate * (double) amount;
+        // 올림/내림 관련된 요구사항이 없으므로 단순 타입 캐스팅
+        super.deposit( (int)interest + super.getBalance() );
     }
 }
