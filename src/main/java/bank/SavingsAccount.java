699f2f3d8316ca8bc03669fb349f98e54e0b45a9
insonggim <insonggim8@gmail.com>
2026-01-21 20:02:13 +0900

    Feat: Test_통과 10개의 단위 테스트 및 개별 테스트 통과

--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -1,4 +1,23 @@
 package bank;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+    double interestRate;
+
+    public SavingsAccount(String name, int balance ,double interestRate) {
+        super(name,balance);
+        setInterestRate(interestRate);
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void deposit(int money) {
+        super.deposit((int) (money * this.interestRate) + getBalance());
+    }
 }
