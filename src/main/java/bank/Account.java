7661393b22834e4af94205d7ba4b69b2d29b634e
ysbeen00@gmail.com <ysbeen00@gmail.com>
2026-01-21 19:47:46 +0900

    feat: 기능 구현 완료

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -1,4 +1,52 @@
 package bank;
 
 public class Account {
+	protected String owner;
+	protected int balance;
+	public Account(String owner) {
+		this(owner,0);
+		
+	}
+	
+	public Account(String owner, int balance) {
+		this.owner = owner;
+		if (balance<0) balance = 0;
+		this.balance = balance;
+		initializedMessage();
+	}
+	
+	public void initializedMessage() {
+		System.out.println("Account created");
+	}
+	
+	void deposit(int amount) {
+		//예금하다.
+		if (validateInputAmount(amount)) balance = balance + amount;
+	}
+	
+	void withdraw(int amount) {
+		//인출하다.
+		if (validateInputAmount(amount) & balance-amount>=0) {
+			balance = balance -amount;
+		}
+		
+	}
+	
+	boolean validateInputAmount(int amount) {
+		if (amount<0) {
+			return false;
+		}
+		return true;
+	}
+	
+	int getBalance() {
+		return balance;
+	}
+	
+	@Override
+	public String toString() {
+		return "%s님의 계좌 잔액: %d".formatted(owner, balance);
+	}
+	
+	
 }
--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -1,4 +1,19 @@
 package bank;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account{
+	private double interestRate;
+
+	public SavingsAccount(String owner, int balance, double interestRate) {
+		super(owner, balance);
+		this.interestRate = interestRate;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	void deposit(int amount) {
+		balance += amount*(interestRate+1); 
+	}
+	
+	
+	
 }
