03800b872031401e59cfdb9f5a192217c4cc6ea6
Woojae-Jeong <woo6388@naver.com>
2026-01-21 20:04:21 +0900

    feat: 구현 완료

--- src/main/java/bank/BankService.java
+++ src/main/java/bank/BankService.java
@@ -1,4 +1,25 @@
 package bank;
 
 public class BankService {
+    private static final int MAX_SIZE = 10;
+
+    private Account[] accounts = new Account[MAX_SIZE];
+    private int existAccountCount = 0;
+
+    void addAccount(Account account) {
+        if(account == null || existAccountCount >= MAX_SIZE)
+            return;
+
+        accounts[existAccountCount++] = account;
+    }
+
+    public int getTotalBalance(){
+        int totalBalance = 0;
+
+        for(int i = 0 ; i < existAccountCount ; i++){
+            totalBalance += accounts[i].getBalance();
+        }
+
+        return totalBalance;
+    }
 }
--- src/main/java/bank/SavingsAccount.java
+++ src/main/java/bank/SavingsAccount.java
@@ -1,4 +1,15 @@
 package bank;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+    private final double interestRate;
+
+    public SavingsAccount(String owner, int balance, double interestRate){
+        super(owner, balance);
+        this.interestRate = 1.0 + interestRate;
+    }
+
+    @Override
+    public void deposit(int amount) {
+        balance = balance + (int)(amount * interestRate);
+    }
 }
